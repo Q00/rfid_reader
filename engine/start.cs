205c51543f82c430dab8958f36769eecf05f7724
Q00 <jqyu.lee@gmail.com>
2019-02-10 16:34:44 +0900

    (feat) add engine

---
+++ engine/start.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace engine
+{
+    public struct rfid_tag_struct
+    {
+        private string serial;
+        private string name;
+        private string price;
+
+        public rfid_tag_struct(string serial, string name, string price)
+        {
+            this.serial = serial;
+            this.name = name;
+            this.price = price;
+        }
+    }
+    public class Start
+    {
+        public List<List<string>> start_loop()
+        {
+            return new List<List<string>>();
+        }
+    }
+
+    
+}
